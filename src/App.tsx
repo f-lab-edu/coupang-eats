c33a9c02138b562ae5135d4f9679a8e43e87981c
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2024-02-09 09:34:20 +0900

    [Feat]초기 개발환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  const arr = [1, 2, 3];
+  arr.forEach(v => {
+    console.log(v);
+  });
+  return (
+    <>
+      <div className="bg-blue-300">
+        <h1>hello world</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
